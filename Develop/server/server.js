c9be02f321764356e610cda18a1d9eea44831795
Trengland <englandvivial@gmail.com>
2023-06-26 16:06:54 -0400

    replaced RESTful APIs with GraphQL and Apollo Server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,53 @@
-const express = require('express');
-const path = require('path');
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('../schema');
+const resolvers = require('./resolvers');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
+const server = new ApolloServer({ typeDefs, resolvers });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  });
 });
+
+
+
+
+
+
+
+
+// // code before i setup graphql and apollo server
+// const express = require('express');
+// const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+// const { ApolloServer } = require('apollo-server');
+// const typeDefs = require('./schema');
+// const resolvers = require('./resolvers');
+
+// const server = new ApolloServer({ typeDefs, resolvers });
+
+// // Start the server
+// server.listen().then(({ url }) => {
+//   console.log(`Server ready at ${url}`);
+// });
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
